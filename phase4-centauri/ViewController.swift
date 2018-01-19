bb67623d6c6b5c1118e75e0550d6147876d25d94
Yusef Nathanson <yusefnathanson@gmail.com>
2018-01-19 14:09:18 -0500

    Initial Commit

---
+++ phase4-centauri/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  phase4-centauri
+//
+//  Created by Yusef Nathanson on 1/19/18.
+//  Copyright © 2018 Yusef Nathanson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
